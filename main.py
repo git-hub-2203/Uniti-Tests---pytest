9bfb76429437d7040435eab6da78ece3efd982ed
BLoomWithDignity <hubg08489@gmail.com>
2026-01-03 12:55:50 -0400

    Versão Funcional -> 3.13

---
+++ main.py
@@ -0,0 +1,11 @@
+def function():
+    x = True
+    y = False
+    z = x + y
+    return z
+
+def main():
+    print(function())
+
+if __name__ == "__main__":
+    main()
---
+++ test_main.py
@@ -0,0 +1,4 @@
+from main import function
+
+def test_function():
+    assert function() == 1
